16d62d990c24f8ab859d8c400e77b815d762a78b
SimonGideon <simongideon918@gmail.com>
2023-10-19 10:50:14 +0300

    Add responsiveness

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,17 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { RiCloseLine } from "react-icons/ri";
+import { logo } from "../assets";
+import { links } from "../assets/constants";
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+    <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+      <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+    </div>
+    </>
+  )
+  }
 
 export default Sidebar;
