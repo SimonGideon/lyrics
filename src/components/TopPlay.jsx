09b393a124eeb8bb3893ed90d921d4b1f77a3ee0
SimonGideon <simongideon918@gmail.com>
2023-10-19 16:41:46 +0300

    top-play imports

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,43 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import PlayPause from "./PlayPause";
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    divRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
+  const topPlays = Array.isArray(data) ? data.slice(0, 5) : [];
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = (song, data, i) => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  return (
+    <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+          <Link to="/top-charts">
+            <p>See more</p>
+          </Link>
+
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TopPlay;
