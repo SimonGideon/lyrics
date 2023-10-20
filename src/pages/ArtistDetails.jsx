b4c155d81fdd0ad805599446b3fd9dc057f0ff53
SimonGideon <simongideon918@gmail.com>
2023-10-20 17:03:17 +0300

    fetch song detail

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+import { DetailsHeader, Error, Loader, ArtistRelatedSongs } from '../components';
 
-import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
+import { useGetArtistDetailsQuery, useGetTopArtistSongsQuery } from '../redux/services/shazamCore';
 
 const ArtistDetails = () => {
   const { id: artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
-
+  const { data: topSongsData, isFetching: isFetchingTopSongs } = useGetTopArtistSongsQuery(artistId);
   if (isFetchingArtistDetails) return <Loader title="Loading artist details..." />;
 
   if (error) return <Error />;
-
   return (
     <div className="flex flex-col">
       <DetailsHeader
         artistId={artistId}
         artistData={artistData?.data[0]}
       />
-
-      <RelatedSongs
-        data={artistData?.data[0].views['top-songs']?.data}
+      <ArtistRelatedSongs
+        data={topSongsData}
         artistId={artistId}
         isPlaying={isPlaying}
         activeSong={activeSong}
