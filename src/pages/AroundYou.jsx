6f93315bf579e68f2c77b6f5e323fb3c76eeb4c2
SimonGideon <simongideon918@gmail.com>
2023-10-21 18:51:57 +0300

    Initial commit

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -11,7 +11,7 @@ const CountryTracks = () => {
   const { data, isFetching, error } = useGetSongByCountryQuery(country);
   useEffect(() => {
     axios
-      .get(`https://geo.ipify.org/api/v2/country?apiKey=at_${VITE_GEO_API_KEY}`)
+      .get("https://geo.ipify.org/api/v2/country?apiKey=at_oda3LsqDv9PwqPiXbOBiL0u9DzKDO")
       .then((res) => setCountry(res?.data?.location?.country))
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
