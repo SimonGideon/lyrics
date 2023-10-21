52529265ec6a20a1e5a794ed71f910f1592b9437
SimonGideon <simongideon918@gmail.com>
2023-10-21 12:59:53 +0300

    get current location-contry

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
-
-const CountryTracks = () => <div>CountryTracks</div>;
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { Error, Loader, SongCard } from '../components';
+const CountryTracks = () =>{
+    const [country, setCountry] = useState('');
+    const [loading, setLoading] = useState(true);
+    const { activeSong, isPlaying} = useSelector((state)=> state.player);
+    console.log(country)
+    useEffect(()=>{
+        // at_oda3LsqDv9PwqPiXbOBiL0u9DzKDO 
+        axios.get('https://geo.ipify.org/api/v2/country?apiKey=at_oda3LsqDv9PwqPiXbOBiL0u9DzKDO')
+        .then((res)=> setCountry(res?.data?.location?.country))
+        .catch((err)=> console.log(err) )
+        .finally(()=> setLoading(false));
+    }, [country])
+    return(
+        <div></div>
+    )
+};
 
 export default CountryTracks;
