99a85c64c823e95bbdf8d0b7e45cbdd489f02161
SimonGideon <simongideon918@gmail.com>
2023-11-03 12:34:36 +0300

    handle keys

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -11,7 +11,8 @@ const CountryTracks = () => {
   const { data, isFetching, error } = useGetSongByCountryQuery(country);
   useEffect(() => {
     axios
-      .get("https://geo.ipify.org/api/v2/country?apiKey=at_oda3LsqDv9PwqPiXbOBiL0u9DzKDO")     .then((res) => setCountry(res?.data?.location?.country))
+    .get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEO_KEY}`)
+      .then((res) => setCountry(res?.data?.location?.country))
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
   }, [country]);
